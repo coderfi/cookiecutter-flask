1d92867fe2ccc9f3106625941c3844f4ad94f98b
coderfi <coderfi@gmail.com>
2014-03-17 02:01:29 -0700

    added wsgi file

---
+++ {{cookiecutter.app_name}}/wsgi.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from {{cookiecutter.app_name}}.app import create_app
+from {{cookiecutter.app_name}}.settings import DevConfig, ProdConfig
+
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+# from tornado import autoreload
+
+import os
+import sys
+
+if os.environ.get("{{cookiecutter.app_name}}_ENV") == 'prod':
+    app = create_app(ProdConfig)
+else:
+    app = create_app(DevConfig)
+
+
+def main(argv=None):
+    port = 5000
+
+    print("Listening on 0.0.0.0:%d" % port)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(port)
+    IOLoop.instance().start()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
